0e3a90bbd93bb2a40b7f935a6eab3a0d5f0de635
Pham-Gia-Huong <phamgiahuong2912@gmail.com>
2019-11-19 14:56:14 +0700

    remove build 32bit

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@
 GOOS=linux GOARCH=amd64 go build -o build/linux-x64/cli-kintone
 GOOS=darwin GOARCH=amd64 go build -o build/macos-x64/cli-kintone
 GOOS=windows GOARCH=amd64 go build -o build/windows-x64/cli-kintone.exe
-GOOS=windows GOARCH=386 go build -o build/windows/cli-kintone.exe
 
 zip -r cli-kintone.zip build
 

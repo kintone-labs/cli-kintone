4b697a6d600661795bb932bc6379b7286acb9f0a
huong <phamgiahuong2012@gmail.com>
2019-12-23 13:28:39 +0700

    fix review unit test #3

--- export_test.go
+++ export_test.go
@@ -48,41 +48,31 @@ func makeTestData(app *kintone.App) error {
 func TestSeekMethod(t *testing.T) {
 	app := newApp()
 	config.Query = ""
-	records, _, err := getRecords(app, []string{"id", "Status"})
+	_, err := getRecords(app, []string{"id", "Status"})
 	if err != nil {
 		t.Error("TestSeekMethod is failed:", err)
 	}
-	for _, record := range records {
-		fmt.Println(record.Fields)
 
-	}
 }
 
 func TestGetRecordsHaveLimitOffset(t *testing.T) {
 	app := newApp()
 	config.Query = "limit 100 offset 0"
-	records, _, err := getRecords(app, []string{"id", "Status"})
+	_, err := getRecords(app, []string{"id", "Status"})
 	if err != nil {
 		t.Error("TestGetRecordsHaveLimitOffset is failed:", err)
 	}
 
-	for _, record := range records {
-		fmt.Println(record.Fields)
-
-	}
 }
 
 func TestGetRecordsHaveQuery(t *testing.T) {
 	app := newApp()
 	config.Query = "order by $id desc"
-	records, _, err := getRecords(app, nil)
+	_, err := getRecords(app, nil)
 	if err != nil {
 		t.Error("TestGetRecordsHaveQuery is failed:", err)
 	}
-	for _, record := range records {
-		fmt.Println(record.Fields)
 
-	}
 }
 
 func TestExport1(t *testing.T) {

1054c16abe077db6285ecea69bdbbda151324b0b
minhhy2801 <minhhy2897@gmail.com>
2020-02-27 08:37:33 +0700

    Add GBK and big5 encoding support for Chinese characters.

--- main.go
+++ main.go
@@ -9,9 +9,11 @@ import (
 
 	"github.com/howeyc/gopass"
 	"github.com/kintone/go-kintone"
-	"golang.org/x/text/encoding"
-	"golang.org/x/text/encoding/japanese"
-	"golang.org/x/text/encoding/unicode"
+  "golang.org/x/text/encoding"
+  "golang.org/x/text/encoding/unicode"
+  "golang.org/x/text/encoding/japanese"
+  "golang.org/x/text/encoding/simplifiedchinese"
+  "golang.org/x/text/encoding/traditionalchinese"
 
 	flags "github.com/jessevdk/go-flags"
 )
@@ -195,7 +197,11 @@ func getEncoding() encoding.Encoding {
 	case "euc-jp":
 		return japanese.EUCJP
 	case "sjis":
-		return japanese.ShiftJIS
+    return japanese.ShiftJIS
+  case "gbk":
+    return simplifiedchinese.GBK
+  case "big5":
+    return traditionalchinese.Big5
 	default:
 		return nil
 	}

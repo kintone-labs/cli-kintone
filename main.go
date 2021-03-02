3ec2d248d7dffcc721c0c8c1f0a2f337e2404f02
Vo Duy Khanh <39976007+josh-vo@users.noreply.github.com>
2021-03-02 16:18:48 +0700

    Update version to v0.12.0

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ import (
 const NAME = "cli-kintone"
 
 // VERSION of this package
-const VERSION = "0.11.3"
+const VERSION = "0.12.0"
 
 // IMPORT_ROW_LIMIT The maximum row will be import
 const IMPORT_ROW_LIMIT = 100
